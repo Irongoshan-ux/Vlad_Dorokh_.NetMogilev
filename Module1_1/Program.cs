f1958f638766f85db435cd72726dbecec6639724
Irongoshan-ux <vlad.doroh@gmail.com>
2019-10-10 17:33:52 +0300

    New project was created. Task 1 was completed.

---
+++ Module1_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Module1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 1, b = 2;
+
+            int temp = a;
+            a = b;
+            b = temp;
+
+            Console.WriteLine($"{a}, {b}");
+        }
+    }
+}
