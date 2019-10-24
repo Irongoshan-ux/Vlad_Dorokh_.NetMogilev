ddefa98885987b8b202e9d43d762f12e7caab7f9
Irongoshan-ux <vlad.doroh@gmail.com>
2019-10-24 21:44:08 +0300

    Module 3 is completed.

---
+++ Module 3/Module3_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+
+            int numberOfNaturalNumbers = random.Next(1, 100);
+            Console.WriteLine("Число: " + numberOfNaturalNumbers);
+
+            int[] evenNaturalNumbers = new int[numberOfNaturalNumbers / 2 + 1];
+
+            Console.Write("Четные натуральные числа: ");
+
+            for (int number = 0, i = 0; number < numberOfNaturalNumbers + 1; number++)
+            {
+                if (number % 2 == 0)
+                {
+                    evenNaturalNumbers[i] = number;
+                    Console.Write(evenNaturalNumbers[i] + " ");
+                    i++;
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Module 3/Module3_7/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите размер массива: ");
+            int arrayLength = int.Parse(Console.ReadLine());
+
+            int[] arrayOfNumbers = new int[arrayLength];
+
+            Random random = new Random();
+
+            Console.WriteLine("Исходный массив:");
+
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
+            {
+                arrayOfNumbers[i] = random.Next(-1000, 1000);
+                Console.Write(arrayOfNumbers[i] + " ");
+            }
+
+            Console.WriteLine("\n\nЧисла, большие стоящих перед ними:");
+
+            for (int i = 1; i < arrayOfNumbers.Length; i++)
+            {
+                if (arrayOfNumbers[i] > arrayOfNumbers[i - 1])
+                {
+                    Console.Write(arrayOfNumbers[i] + " ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
